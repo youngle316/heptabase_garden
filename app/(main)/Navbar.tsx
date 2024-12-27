1320fcddfe3318eb40e53d50a7ef8396342e87b8
杨乐乐 <youngle316@gmail.com>
2024-12-27 10:05:46 +0800

    feat: change logo

--- app/(main)/Navbar.tsx
+++ app/(main)/Navbar.tsx
@@ -1,6 +1,5 @@
 "use client";
-import Logo from "@/app/logo.png";
-import Image from "next/image";
+import { Leaf } from "lucide-react";
 
 export default function Navbar() {
   const handleLogoClick = () => {
@@ -13,14 +12,7 @@ export default function Navbar() {
   return (
     <nav className="sticky top-0 z-50 flex items-center justify-between border-zinc-300 border-b px-8 py-2">
       <div onClick={handleLogoClick}>
-        <Image
-          width={24}
-          height={24}
-          priority
-          src={Logo}
-          alt="Follow me on Github"
-          className="cursor-pointer"
-        />
+        <Leaf className="cursor-pointer" />
       </div>
       <div>theme</div>
     </nav>
