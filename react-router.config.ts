f8b01bdb7d88a7d2cdd6f0f5e4b0edb31ca2355b
杨乐乐 <youngle316@gmail.com>
2025-08-26 12:04:09 +0800

    feat: cloudflare

--- react-router.config.ts
+++ react-router.config.ts
@@ -5,4 +5,7 @@ export default {
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
   prerender: true,
+  future: {
+    unstable_viteEnvironmentApi: true,
+  },
 } satisfies Config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+import { cloudflare } from "@cloudflare/vite-plugin";
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [cloudflare({ viteEnvironment: { name: "ssr" } }), reactRouter(), tsconfigPaths()],
 });
